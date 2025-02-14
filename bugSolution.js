bcf1858755911e5146c2c82cc4ce71ec2658062b
BugHunter-X <askzenai@gmail.com>
2025-02-14 01:34:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,12 @@
+```javascript
+// app/not-found.js
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for could not be found.</p>
+    </div>
+  );
+}
+```
